836fa3ff349509d40f2905555c7973b7057cb8d5
Andrew <a.c.gerrie@gmail.com>
2016-06-23 20:40:29 +0100

    First Commit
    First commit with map loading on ios

---
+++ AlmostHereXamarin/AlmostHereMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace AlmostHereXamarin
+{
+	public class AlmostHereMap : ContentPage
+	{
+		public AlmostHereMap ()
+        {
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                        new Position(1, 1), Distance.FromMiles(1000)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
+    }
+}
+
